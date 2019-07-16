6a2b5b94cdff90002a6db8d8181da8870e4000d7
Nuxii <nuxii09@gmail.com>
2019-07-16 20:38:22 +0100

    Added display to token struct.

--- src/Runner/token.rs
+++ src/Runner/token.rs
@@ -1,7 +1,20 @@
+use std::fmt;
+
 /// A struct which stores information about a token.
 pub struct Token {
     pub ident : String,
     pub value : String,
     pub line : usize,
     pub column : usize
+}
+impl fmt::Display for Token {
+    /// Formats the contents of this token.
+    #[allow(dead_code)]
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[ident={}, value={}, line={}, column={}]",
+                self.ident,
+                self.value,
+                self.line,
+                self.column)
+    }
 }
