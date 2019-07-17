efdb8e1c08ef86e8c185b45e42d52ca176322b15
Nuxii <nuxii09@gmail.com>
2019-07-17 22:52:15 +0100

    Updated .gitignore

--- src/Runner/mod.rs
+++
@@ -1,6 +0,0 @@
-// expose submodules
-pub mod token;
-pub mod syntax_tree;
-pub mod better_lexer;
-pub mod lexer;
-pub mod parser;
--- src/Runner/syntax_tree.rs
+++
@@ -1,5 +0,0 @@
-/// A recursive enum used to express an abstract syntax tree.
-pub enum SExpression<T> {
-    Nil,
-    List(T, Vec<SExpression<T>>)
-}
