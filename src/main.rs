2550463a5e46e11c52c21d58fef0c3f6b88fb593
Nuxii <nuxii09@gmail.com>
2019-07-19 01:21:40 +0100

    Updated example code in main to test for new language tokens.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,22 @@ use runner::lexer::Lexer;
 
 fn main() {
     let lexer : Lexer = language::generate_lexer();
-    match lexer.lex("if 12.3") {
+    match lexer.lex("
+            'exit
+            if (1st_variable_name) {
+                repeat {
+                    '' this is a comment
+                    '{
+                        this is also
+                        a comment
+                    }'
+                    \"this is a string\"
+                    1234
+                    5.678
+                    .910
+                    11.
+                } until (2nd_variable_name);
+            }") {
         Some(tokens) => {
             for token in &tokens {
                 println!("{}", token);
