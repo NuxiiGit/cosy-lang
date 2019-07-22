ddd00b50906f205605614a9383e0603f32e87088
Nuxii <nuxii09@gmail.com>
2019-07-22 17:06:06 +0100

    Converted token to enum.

--- src/Runner/token.rs
+++ src/Runner/token.rs
@@ -1,51 +1,18 @@
-/// A struct which stores token data.
+/// An enum which stores token data.
 #[allow(dead_code)]
-pub struct Token {
-    ident : String,
-    value : Option<String>
-}
-impl Token {
-    /// Constructs a new token.
-    #[allow(dead_code)]
-    pub fn new(ident : &str, value : Option<&str>) -> Token {
-        let value : Option<String> = if let Some(value) = value {
-            Some(value
-                    .to_owned()
-                    .escape_debug()
-                    .collect())
-        } else {
-            None
-        };
-        Token {
-            ident : ident.to_owned(),
-            value : value
-        }
-    }
-
-    /// Returns the name of this token.
-    #[allow(dead_code)]
-    pub fn name(&self) -> String {
-        self.ident.to_owned()
-    }
-
-    /// Returns the value of this token.
-    #[allow(dead_code)]
-    pub fn value(&self) -> Option<String> {
-        if let Some(x) = &self.value {
-            Some(x.to_owned())
-        } else {
-            None
-        }
-    }
+#[derive(Debug)]
+pub enum Token {
+    Symbol(String),
+    Keyword(String),
+    Ident(String),
+    Label(String),
+    Numb(String),
+    Str(String)
 }
 impl std::fmt::Display for Token {
     /// Formats the contents of this token.
     #[allow(dead_code)]
     fn fmt(&self, f : &mut std::fmt::Formatter) -> std::fmt::Result {
-        if let Some(x) = &self.value {
-            write!(f, "Token({}, {})", self.ident, x)
-        } else {
-            write!(f, "Token({})", self.ident)
-        }
+        write!(f, "{:?}", self)
     }
 }
