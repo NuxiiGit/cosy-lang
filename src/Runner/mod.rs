43f753d1986f971eb75e881f44741accac4e2366
Nuxii <nuxii09@gmail.com>
2019-09-02 18:43:01 +0100

    Clean slate.

--- src/Runner/mod.rs
+++
@@ -1,5 +0,0 @@
-// Expose submodules
-pub mod token;
-pub mod lexer;
-pub mod parser;
-pub mod syntax_tree;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
-mod runner;
 
-use runner::lexer;
-use runner::parser;
-use std::time::Instant;
+
+//mod runner;
+
+//use runner::lexer;
+//use runner::parser;
+//use std::time::Instant;
 
 fn main() {
-    let now = Instant::now();
-    match lexer::lex(r#"--(-1 + 4*(3)/8 - 1/2)"#) {
-        Ok(tokens) => {
-            println!("Success!");
-            for token in &tokens {
-                println!("{}", token);
-            }
-            match parser::parse(&tokens) {
-                Ok(expr) => {
-                    println!("{}", expr);
-                },
-                Err(msg) => {
-                    println!("{}", msg);
-                }
-            }
-        },
-        Err(msg) => println!("Error! {}", msg)
+    /*let now = Instant::now();
+    let tokens = lexer::lex(r#"-(-1 + 4*(3)/8 - 1/2)"#);
+    for token in &tokens {
+        println!("{}", token);
     }
+    /*match parser::parse(&tokens) {
+        Ok(expr) => {
+            println!("{}", expr);
+        },
+        Err(msg) => {
+            println!("{}", msg);
+        }
+    }*/
     let duration = now.elapsed().as_micros();
     let duration_s : f64 = (duration as f64) / 1000000.0;
-    println!("Time: {} s ({} Ms)", duration_s, duration);
+    println!("Time: {} s ({} Ms)", duration_s, duration);*/
 }
