20e36637b73afc609e1c191b6bd5a05df5b0046c
Nuxii <nuxii09@gmail.com>
2019-09-05 15:49:15 +0100

    Progress on the lexer revamp.

--- src/Runner/lexer_new.rs
+++ src/Runner/lexer_new.rs
@@ -3,46 +3,93 @@
 use super::token::*;
 use super::error::*;
 use std::str::CharIndices;
-use std::iter::Peekable;
 
 /// A struct which encapsulates the state of the scanner.
 pub struct Lexer<'a> {
-    scanner : Peekable<Scanner<'a>>,
-    errors : &'a mut Vec<Error<'a>>
+    context : &'a str,
+    scanner : CharIndices<'a>,
+    errors : &'a mut Vec<Error<'a>>,
+    next : Option<(usize, char)>,
+    row : usize,
+    column : usize
 }
 impl<'a> Lexer<'a> {
     /// Construct a new scanner.
     pub fn new(context : &'a str, errors : &'a mut Vec<Error<'a>>) -> Lexer<'a> {
+        let mut scanner : CharIndices = context.char_indices();
+        let first : Option<(usize, char)> = scanner.next();
         Lexer {
-            scanner : Scanner::new(context)
-                    .peekable(),
+            context : context,
+            scanner : scanner,
+            errors : errors,
+            next : first,
             row : 0,
-            column : 0,
-            errors : errors
+            column : 0
         }
     }
 
     /// Push an error onto the error list.
     fn lexer_error(&mut self, message : &'static str) {
-        self.errors.push(Error::new(message, 
-                scanner.row(), scanner.column()));
+        self.errors.push(Error::new(message,
+                self.row, self.column));
     }
 
     /// Create a new token with the current row and column numbers.
     fn create_token(&self, flavour : TokenType<'a>) -> Token<'a> {
         Token::new(flavour, 
-                scanner.row(), scanner.column())
+                self.row, self.column)
     }
 
     /// Drop the whitespace.
     fn drop_whitespace(&mut self) {
-        while let Some((_, c)) = self.scanner.peek() {
-            if c.is_whitespace() {
-                self.scanner.next();
+        while let Some(x) = self.peek_char() {
+            if x.is_whitespace() {
+                self.next_char();
             } else {
                 break;
             }
         }
+    }
+
+    /// Move to the next character.
+    fn next_char(&mut self) -> Option<char> {
+        let (_, x) = self.next_charindex()?;
+        Some(x)
+    }
+
+    /// Move to the next character-index pair.
+    fn next_charindex(&mut self) -> Option<(usize, char)> {
+        let next : Option<(usize, char)> = self.next;
+        if let Some((_, x)) = next {
+            if let '\n' = x {
+                self.row += 1;
+                self.column = 0;
+            } else {
+                self.column += 1;
+            }
+            self.next = self.scanner.next();
+        }
+        next
+    }
+
+    /// Peek at the next character.
+    fn peek_char(&self) -> Option<char> {
+        let (_, x) = self.peek_charindex()?;
+        Some(x)
+    }
+
+    /// Peek at the next index. Returns `context.len()` if the end is reached.
+    fn peek_index(&self) -> usize {
+        if let Some((i, _)) = self.peek_charindex() {
+            i
+        } else {
+            self.context.len()
+        }
+    }
+
+    /// Peek at the next character-index pair.
+    fn peek_charindex(&self) -> Option<(usize, char)> {
+        self.next
     }
 }
 impl<'a> Iterator for Lexer<'a> {
@@ -50,58 +97,10 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.drop_whitespace();
-        let (start, c) : (usize, char) = self.scanner.next()?;
+        let (c, mut start) = self.scanner.next()?;
         match c {
             // match nothing
             _ => None
         }
-    }
-}
-
-/// A struct which allows you to iterate and slice `&str` types.
-pub struct Scanner<'a> {
-    context : &'a str,
-    chars : CharIndices<'a>,
-    row : usize,
-    column : usize
-}
-impl<'a> Scanner<'a> {
-    /// Construct a new scanner.
-    pub fn new(context : &str) -> Scanner {
-        Scanner {
-            context : context,
-            chars : context.char_indices(),
-            row : 0,
-            column : 0,
-        }
-    }
-
-    /// Returns a slice of the scanner context.
-    pub fn substring(&self, start : usize, end : usize) -> &'a str {
-        &self.context[start..end]
-    }
-
-    /// Returns the current row of the scanner.
-    pub fn row(&mut self) -> usize {
-        self.row
-    }
-
-    /// Returns the current column of the scanner.
-    pub fn column(&mut self) -> usize {
-        self.column
-    }
-}
-impl<'a> Iterator for Scanner<'a> {
-    type Item = (usize, char);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let next : (usize, char) = self.chars.next()?;
-        if let (_, '\n') = next {
-            self.row += 1;
-            self.column = 0;
-        } else {
-            self.column += 1;
-        }
-        Some(next)
     }
 }
