bce6dacbb9ce5dd78735879cd461c1df585b2620
Nuxii <nuxii09@gmail.com>
2019-09-19 00:38:49 +0100

    Added more to interpreter framework.

--- src/Runner/interpreter.rs
+++ src/Runner/interpreter.rs
@@ -6,23 +6,44 @@ use super::essentials::{
     syntax_tree::*
 };
 
-use std::iter::Peekable;
-
 /// A struct which encapsulates the state of the evaluator.
-struct Interpreter<'a> {
-    program : Statement<'a>
-}
-impl<'a> Interpreter<'a> {
+struct Interpreter;
+impl<'a> Interpreter {
     /// Constructs a new evaluator.
-    pub fn new(program : Statement<'a>) -> Interpreter<'a> {
-        Interpreter {
-            program
+    pub fn new() -> Interpreter {
+        Interpreter {}
+    }
+
+    /// Executes this program and returns a value.
+    pub fn execute(mut self, program : &Statement<'a>) -> Option<Value> {
+        self.touch_statement(program)
+    }
+
+    /// Evaluates a statement.
+    fn touch_statement(&mut self, statement : &Statement<'a>) -> Option<Value> {
+        match statement {
+            Statement::ExpressionStatement { expr } => self.touch_expression(expr),
+            _ => panic!()
         }
     }
 
-    pub fn execute(mut self) -> Option<Value> {
+    /// Evaluates an expression.
+    fn touch_expression(&mut self, expr : &Expr<'a>) -> Option<Value> {
+        match expr {
+            Expr::Literal { value } => self.touch_expression_literal(value),
+            /*Expr::Variable { ident } => None,
+            Expr::Unary { operator, right } => None,
+            Expr::Binary { operator, left, right } => None,
+            Expr::Member { left, field } => None,*/
+            _ => panic!()
+        }
+    }
+
+    /// Evaluates a literal.
+    fn touch_expression_literal(&mut self, token : &Token<'a>) -> Option<Value> {
         None
     }
+
 }
 
 /// An enum which describes the different types of value.
