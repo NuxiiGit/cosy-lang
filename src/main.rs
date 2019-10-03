a6370cd25e524a5bd35b455289c0aa5a4d3d0174
Nuxii <nuxii09@gmail.com>
2019-10-03 17:01:00 +0100

    Updated main.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,25 @@ use runner::compiler::scanner::Tokeniser;
 use runner::compiler::parser::Builder;
 use runner::evaluator::interpreter::Interpreter;
 
+use std::time::Instant;
+
 fn main() {
+    let t = Instant::now();
+    println!("\nCompiling...");
+    // compile
     match r#"255"#.tokenise().into_ast() {
         Ok(ast) => {
-            println!("Tree:\n{:#?}", ast);
+            // record time
+            let dt = t.elapsed();
+            println!("\nCompile Time:\n{} ms ({} Ms)", 
+                    dt.as_millis(), dt.as_micros());
+            // interpret
+            println!("\nSyntax Tree:\n{:#?}", ast);
             match Interpreter::interpret(ast) {
-                Ok(value) => println!("Value: {:?}", value),
-                Err(e) => println!("{}", e)
+                Ok(value) => println!("\nInterpreter Result:\n{:?}", value),
+                Err(e) => println!("\n{}", e)
             }
         },
-        Err(e) => println!("{}", e)
+        Err(e) => println!("\n{}", e)
     }
 }
