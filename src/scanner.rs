a692cc6349a090c0ff1827a78c5f0a9935975f3a
Nuxii <nuxii09@gmail.com>
2019-11-22 00:24:31 +0000

    Added drafts for scanner.

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,4 @@
+use super::error::Error;
 use super::token::{
     Token,
     TokenKind,
@@ -10,11 +11,25 @@ use std::str::CharIndices;
 
 /// An iterator over a string slice, which produces `Token`s.
 pub struct Lexer<'a> {
+    scanner : StrScanner<'a>
+}
+impl<'a> Lexer<'a> {
+
+}
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Result<Token<'a>, Error<'a>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+/// A structure over a string slice which produces individual `Span`s of tokens.
+pub struct StrScanner<'a> {
     context : &'a str,
     chars : Peekable<CharIndices<'a>>,
     row : usize,
     column : usize
 }
-impl<'a> Lexer<'a> {
+impl<'a> StrScanner<'a> {
 
 }
