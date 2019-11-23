e49b83d29c2f62535d47627865bbfc180a29c80a
Nuxii <nuxii09@gmail.com>
2019-11-23 19:39:43 +0000

    Updated scanner.

--- src/scanner.rs
+++ src/scanner.rs
@@ -14,7 +14,10 @@ pub struct Lexer<'a> {
     scanner : StrScanner<'a>
 }
 impl<'a> Lexer<'a> {
-
+    /// Create a new lexer.
+    pub fn new(scanner : StrScanner<'a>) -> Lexer<'a> {
+        Lexer { scanner }
+    }
 }
 impl<'a> Iterator for Lexer<'a> {
     type Item = Result<Token<'a>, Error<'a>>;
@@ -31,5 +34,54 @@ pub struct StrScanner<'a> {
     column : usize
 }
 impl<'a> StrScanner<'a> {
+    /// Create a new scanner from this string slice.
+    pub fn from(context : &'a str) -> StrScanner<'a> {
+        StrScanner {
+            context,
+            chars : context
+                    .char_indices()
+                    .peekable(),
+            row : 1,
+            column : 0
+        }
+    }
 
+    /// Returns the current column of the scanner.
+    fn column(&self) -> usize {
+        self.column
+    }
+
+    /// Returns the current row of the scanner.
+    fn row(&self) -> usize {
+        self.row
+    }
+
+    /// Peek at the next character.
+    fn chr(&mut self) -> Option<char> {
+        let (_, x) = self.chars.peek()?;
+        Some(*x)
+    }
+
+    /// Peek at the next index.
+    /// Returns `str.len()` if the end is reached.
+    fn pos(&mut self) -> usize {
+        if let Some((i, _)) = self.chars.peek() {
+            *i
+        } else {
+            self.context.len()
+        }
+    }
+
+    /// Move to the next character.
+    fn advance(&mut self) -> Option<char> {
+        let (_, x) = self.chars.next()?;
+        if x == '\n' {
+            // move to new line
+            self.row += 1;
+            self.column = 1;
+        } else {
+            self.column += 1;
+        }
+        Some(x)
+    }
 }
