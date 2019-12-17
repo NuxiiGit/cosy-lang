fe8554f6c25a8f72511d88b7cd8d64038185b82d
Nuxii <nuxii09@gmail.com>
2019-12-17 15:48:52 +0000

    Added basic interpreter values.

---
+++ src/evaluator.rs
@@ -0,0 +1,23 @@
+use crate::diagnostics::*;
+use crate::syntax::{
+    token::*,
+    ast::*
+};
+
+use std::collections::hash_map::HashMap;
+
+/// Takes a lexer and uses it to construct a parse tree.
+pub struct Interpreter {
+}
+
+/// An enum of value types recognised by the interpreter.
+#[derive(Clone)]
+pub enum Value {
+    Integer(i64),
+    Real(f64),
+    Char(char),
+    Function {
+        capture : Box<Value>,
+        func : fn(Value, Value) -> Value
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod diagnostics;
 pub mod syntax;
 pub mod lexer;
 pub mod parser;
+pub mod evaluator;
