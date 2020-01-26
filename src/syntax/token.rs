a9867e173cb8fca53236cc0d1e27bd55b415d1d3
NuxiiGit <nuxii09@gmail.com>
2020-01-26 23:23:53 +0000

    Added is_<whatever> methods to TokenKind.

--- src/syntax/token.rs
+++ src/syntax/token.rs
@@ -19,6 +19,47 @@ pub enum TokenKind {
     Documentation,
     Unknown
 }
+impl TokenKind {
+    /// Returns `true` of the token is a keyword.
+    pub fn is_keyword(&self) -> bool {
+        if let TokenKind::Keyword(..) = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is an identifier.
+    pub fn is_identifier(&self) -> bool {
+        if let TokenKind::Identifier = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is an operator.
+    pub fn is_operator(&self) -> bool {
+        if let TokenKind::Operator(..) = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is a literal.
+    pub fn is_literal(&self) -> bool {
+        if let TokenKind::Literal(..) = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is the end of the file.
+    pub fn is_eof(&self) -> bool {
+        if let TokenKind::EoF = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is a compiler directive.
+    pub fn is_directive(&self) -> bool {
+        if let TokenKind::Directive = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is documentation.
+    pub fn is_documentation(&self) -> bool {
+        if let TokenKind::Documentation = self { true } else { false }
+    }
+
+    /// Returns `true` of the token is unknown.
+    pub fn is_unknown(&self) -> bool {
+        if let TokenKind::Unknown = self { true } else { false }
+    }
+}
 
 /// An enum which describes available keyword types.
 #[derive(PartialEq, Debug, Clone)]
