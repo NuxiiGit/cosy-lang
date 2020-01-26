27ec2f8424eaad8a35620917396ae2868c0b7225
NuxiiGit <nuxii09@gmail.com>
2020-01-26 21:41:30 +0000

    Removed error system.

--- src/diagnostics/mod.rs
+++ src/diagnostics/mod.rs
@@ -1,1 +1,18 @@
-pub mod error;
+use crate::syntax::span::Span;
+
+use std::fmt;
+use std::error;
+
+/// A struct which stores error information.
+#[derive(Debug)]
+pub struct Error {
+    pub reason : &'static str,
+    pub span : Span
+}
+impl fmt::Display for Error {
+    fn fmt(&self, out : &mut fmt::Formatter) -> fmt::Result {
+        write!(out, "{}: {}",
+                self.span, self.reason)
+    }
+}
+impl error::Error for Error {}
