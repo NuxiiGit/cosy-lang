b0e65d41be2508610d41e96963456c91c17aa44d
NuxiiGit <nuxii09@gmail.com>
2020-01-30 20:22:32 +0000

    More refactors to diagnostics.

--- src/diagnostics/error.rs
+++ src/diagnostics/error.rs
@@ -3,6 +3,46 @@ use crate::syntax::token::Token;
 use std::fmt;
 use std::error;
 
+/// A struct which stores compiler session information.
+pub struct Session {
+    errors : Vec<Error>,
+    warnings : Vec<Error>
+}
+impl Session {
+    /// Creates a new empty session.
+    pub fn new() -> Self {
+        Self {
+            errors : Vec::new(),
+            warnings : Vec::new()
+        }
+    }
+
+    /// Adds a new error to the session.
+    pub fn add_error(&mut self, error : Error) {
+        self.errors.push(error);
+    }
+
+    /// Adds a new warning to the session.
+    pub fn add_warning(&mut self, error : Error) {
+        self.warnings.push(error);
+    }
+
+    /// Returns whether a fatal error occured.
+    pub fn is_fatal(&self) -> bool {
+        !self.errors.is_empty()
+    }
+
+    /// Returns a reference to all fatal errors.
+    pub fn errors(&self) -> &[Error] {
+        &self.errors
+    }
+
+    /// Returns a reference to all warnings.
+    pub fn warnings(&self) -> &[Error] {
+        &self.warnings
+    }
+}
+
 /// A struct which stores error information.
 #[derive(Debug)]
 pub struct Error {
--- src/diagnostics/mod.rs
+++ src/diagnostics/mod.rs
@@ -1,71 +1,31 @@
 pub mod error;
 
-use crate::syntax::token::Token;
-
-use error::Error;
-
-/// A struct which stores compiler session information.
-pub struct Session {
-    errors : Vec<Error>,
-    warnings : Vec<Error>
-}
-impl Session {
-    /// Creates a new empty session.
-    pub fn new() -> Self {
-        Self {
-            errors : Vec::new(),
-            warnings : Vec::new()
-        }
-    }
-
-    /// Returns whether a fatal error occured.
-    pub fn is_fatal(&self) -> bool {
-        !self.errors.is_empty()
-    }
-
-    /// Returns a reference to all fatal errors.
-    pub fn errors(&self) -> &[Error] {
-        &self.errors
-    }
-
-    /// Returns a reference to all warnings.
-    pub fn warnings(&self) -> &[Error] {
-        &self.warnings
-    }
-}
+use std::cmp::PartialEq;
 
 /// Builds a diagnostic which can be used to throw errors or check validity.
-pub struct DiagnosticBuilder<T> {
-    error_occured : bool,
+pub struct Diagnostic<T : PartialEq> {
+    invalid : bool,
     value : T
 }
-impl<T> DiagnosticBuilder<T> {
+impl<T : PartialEq> Diagnostic<T> {
     /// Creates a new diagnostic builder with this value.
     pub fn from(value : T) -> Self {
         Self {
-            error_occured : false,
+            invalid : true,
             value
         }
     }
 
-    /// Consumes the diagnostic and returns a boolean value depending on whether the value is valid.
-    pub fn check(self) -> bool {
-        self.error_occured
+    /// Validates the value if it is equal to the expected value.
+    pub fn expects(mut self, value : T) -> Self {
+        if self.invalid && self.value == value {
+            self.invalid = true;
+        }
+        self
     }
 
-    /// Reports a warning to the session if the value was invalid according to the current diagnostic.
-    pub fn warn(self, mut sess : Session, error : Error) -> Self {
-        if self.error_occured {
-            sess.warnings.push(error);
-        }
-        self
-    }
-    
-    /// Reports a fatal error to the session if the value was invalid according to the current diagnostic.
-    pub fn error(self, mut sess : Session, error : Error) -> Self {
-        if self.error_occured {
-            sess.warnings.push(error);
-        }
-        self
+    /// Consumes the diagnostic and returns a boolean value depending on whether the value is valid.
+    pub fn check(self) -> bool {
+        !self.invalid
     }
 }
