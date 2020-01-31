2cef08e767c86505f28e9e052f53237113285ce7
NuxiiGit <nuxii09@gmail.com>
2020-01-31 21:17:31 +0000

    Bleh..

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,2 +1,23 @@
 pub mod scanner;
 
+use crate::diagnostics::error::{ Error, Session };
+use crate::syntax::token::*;
+
+use scanner::Scanner;
+
+/// An iterator over a string slice which produces `Token`s.
+pub struct Lexer<'a, 'b> {
+    scanner : Scanner<'a>,
+    sess : &'b Session,
+    eof : bool
+}
+impl<'a, 'b> Lexer<'a, 'b> {
+    /// Creates a new lexer from this string scanner and parser session.
+    pub fn from(scanner : Scanner<'a>, sess : &'b Session) -> Self {
+        Lexer {
+            scanner,
+            sess,
+            eof : false
+        }
+    }
+}
