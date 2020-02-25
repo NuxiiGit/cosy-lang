6f5c521921670e8bbda4d650c270fb97bb7b1b32
NuxiiGit <nuxii09@gmail.com>
2020-02-25 23:48:54 +0000

    Completely merged parser and lexer.

--- src/libcosyc_parser/src/lib.rs
+++ src/libcosyc_parser/src/lib.rs
@@ -6,7 +6,7 @@ use std::iter::Peekable;
 
 pub struct Parser<'a, 'b> {
     reader : StringReader<'a>,
-    issues : &'b IssueTracker<'a>
+    issues : &'b mut IssueTracker<'a>
 }
 impl<'a, 'b> Parser<'a, 'b> {
     /// Creates a new parser from this string reader and issue tracker.
@@ -14,14 +14,138 @@ impl<'a, 'b> Parser<'a, 'b> {
         Self { reader, issues }
     }
 
+    /// Returns the next token.
+    fn next(&mut self) -> Option<Token<'a>> {
+        'search:
+        loop {
+            self.reader.reset_context();
+            let next = self.reader.next();
+            let peek = self.reader.peek();
+            let kind = match next {
+                x if x.is_valid_whitespace() => {
+                    while self.reader.peek()
+                            .is_valid_whitespace() {
+                        self.reader.next();
+                    }
+                    continue 'search;
+                },
+                CharKind::Minus if peek == CharKind::Minus => {
+                    // line comments
+                    while !self.reader.peek()
+                            .is_valid_ending() {
+                        self.reader.next();
+                    }
+                    continue 'search;
+                },
+                CharKind::Minus if peek == CharKind::Minus => {
+                    // block comments
+                    self.reader.next();
+                    let mut nests = 1;
+                    loop {
+                        let next = self.reader.next();
+                        let peek = self.reader.peek();
+                        match (next, peek) {
+                            (_, CharKind::EoF) => {
+                                self.issues.report(self.make_error(ErrorKind::Warning, "unterminated block comment"));
+                                continue 'search;
+                            },
+                            (CharKind::LeftBrace, CharKind::Minus) => {
+                                self.reader.next();
+                                nests += 1
+                            },
+                            (CharKind::Minus, CharKind::RightBrace) => {
+                                self.reader.next();
+                                if nests == 1 {
+                                    continue 'search;
+                                } else {
+                                    nests -= 1;
+                                }
+                            },
+                            _ => ()
+                        }
+                    }
+                },
+                x if x.is_valid_digit() => {
+                    while self.reader.peek()
+                            .is_valid_digit() {
+                        self.reader.next();
+                    }
+                    TokenKind::Literal(LiteralKind::Integer)
+                },
+                x if x.is_valid_graphic() => {
+                    while self.reader.peek()
+                            .is_valid_graphic() {
+                        self.reader.next();
+                    }
+                    match self.reader.substr() {
+                        "var" => TokenKind::Keyword(KeywordKind::Var),
+                        "if" => TokenKind::Keyword(KeywordKind::If),
+                        "else" => TokenKind::Keyword(KeywordKind::Else),
+                        _ => TokenKind::Identifier
+                    }
+                },
+                x if x.is_valid_operator() => {
+                    let kind = match x {
+                        CharKind::Bar => OperatorKind::Bar,
+                        CharKind::Caret => OperatorKind::Caret,
+                        CharKind::Ampersand => OperatorKind::Ampersand,
+                        CharKind::Bang => OperatorKind::Bang,
+                        CharKind::Equals => OperatorKind::Equals,
+                        CharKind::LessThan => OperatorKind::LessThan,
+                        CharKind::GreaterThan => OperatorKind::GreaterThan,
+                        CharKind::Plus => OperatorKind::Plus,
+                        CharKind::Minus => OperatorKind::Minus,
+                        CharKind::Asterisk => OperatorKind::Asterisk,
+                        CharKind::ForwardSlash => OperatorKind::ForwardSlash,
+                        CharKind::Percent => OperatorKind::Percent,
+                        _ => OperatorKind::Other
+                    };
+                    while self.reader.peek()
+                            .is_valid_operator() {
+                        self.reader.next();
+                    }
+                    match self.reader.substr() {
+                        _ => TokenKind::Operator(kind)
+                    }
+                },
+                CharKind::LeftParen => TokenKind::Symbol(SymbolKind::LeftParen),
+                CharKind::RightParen => TokenKind::Symbol(SymbolKind::RightParen),
+                CharKind::LeftBrace => TokenKind::Symbol(SymbolKind::LeftBrace),
+                CharKind::RightBrace => TokenKind::Symbol(SymbolKind::RightBrace),
+                CharKind::SemiColon => TokenKind::Symbol(SymbolKind::SemiColon),
+                CharKind::Dollar => TokenKind::Symbol(SymbolKind::Dollar),
+                CharKind::Backtick => TokenKind::Symbol(SymbolKind::Backtick),
+                CharKind::Hashtag => {
+                    if let CharKind::Graphic = self.reader.peek() {
+                        self.reader.next();
+                        while let CharKind::Graphic = self.reader.peek() {
+                            self.reader.next();
+                        }
+                        TokenKind::Directive
+                    } else {
+                        self.issues.report(self.make_error(ErrorKind::Fatal, "expected graphic after hashtag symbol"));
+                        break 'search None;
+                    }
+                },
+                CharKind::Address => TokenKind::Symbol(SymbolKind::Address),
+                CharKind::EoF => TokenKind::EoF,
+                _ => {
+                    self.issues.report(self.make_error(ErrorKind::Fatal, "expected graphic after hashtag symbol"));
+                    break 'search None;
+                }
+            };
+            break 'search Some(self.make_token(kind));
+        }
+    }
+
     /// Creates a new error of this kind and reason.
-    pub fn make_error(&self, kind : ErrorKind, reason : &'static str) -> Error<'a> {
+    fn make_error(&self, kind : ErrorKind, reason : &'static str) -> Error<'a> {
         let token = self.make_token(TokenKind::Unknown);
         Error { reason, token, kind }
     }
 
     /// Creates a new token of this kind.
-    pub fn make_token(&self, kind : TokenKind) -> Token<'a> {
+    fn make_token(&self, kind : TokenKind) -> Token<'a> {
         let context = self.reader.context();
         Token { context, kind }
     }
@@ -80,7 +204,7 @@ impl<'a> StringReader<'a> {
     }
 
     /// Clears the current substring.
-    pub fn clear(&mut self) {
+    pub fn reset_context(&mut self) {
         self.byte_start = self.byte_end;
     }
 
