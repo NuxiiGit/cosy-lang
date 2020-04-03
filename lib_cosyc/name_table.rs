4cd7ada738689538c60c12ca94a29554ce3d21b2
NuxiiGit <nuxii09@gmail.com>
2020-04-03 23:36:29 +0100

    Updated name table implementation.

--- lib_cosyc/name_table.rs
+++ lib_cosyc/name_table.rs
@@ -1,35 +1,44 @@
 use std::collections::HashMap;
 
-/// A struct which maps string slices to identifier keys.
-/// Identifier keys are not hashes of the slice, and are,
-/// in fact, enumerations.
+/// A struct which maps string slices to identifier ids.
+/// Identifier ids are just arbitrary enumerations.
 pub struct NameTable<'a> {
     current_key : Identifier,
-    name_table : HashMap<&'a str, Identifier>
+    name_table : HashMap<&'a str, Identifier>,
+    key_table : HashMap<Identifier, &'a str>
 }
 impl<'a> NameTable<'a> {
     /// Creates an empty name table.
     pub fn new() -> Self {
         Self {
             current_key : 0,
-            name_table : HashMap::new()
+            name_table : HashMap::new(),
+            key_table : HashMap::new()
         }
     }
 
-    /// Inserts an identifier into the name table and
-    /// returns its key.
-    pub fn insert(&mut self, ident : &'a str) -> Identifier {
+    /// Inserts an identifier into the name table and returns its id.
+    /// Behaves exactly the same as `get` if the identifier already exists.
+    pub fn add_name(&mut self, ident : &'a str) -> Identifier {
         let key = self.current_key;
         self.current_key += 1;
         self.name_table.insert(ident, key);
+        self.key_table.insert(key, ident);
         key
     }
 
-    /// Attempts to find the identifier key for this name.
+    /// Attempts to find the id of the identifier with this name.
     /// # Errors
     /// Returns `None` if the identifier does not exist.
-    pub fn get(&mut self, ident : &'a str) -> Option<Identifier> {
-        Some(self.name_table.get(ident)?.to_owned())
+    pub fn get_id(&mut self, ident : &'a str) -> Option<Identifier> {
+        Some(*self.name_table.get(ident)?)
+    }
+
+    /// Attempts to find the name of an identifier with this id.
+    /// # Errors
+    /// Returns `None` if an identifier with this id does not exist.
+    pub fn get_name(&mut self, id : &Identifier) -> Option<&'a str> {
+        Some(*self.key_table.get(id)?)
     }
 }
 
