69dc2ea0e0971092c9dd795ac9d73472b9cb685a
NuxiiGit <nuxii09@gmail.com>
2020-04-06 22:08:20 +0100

    Refactors to token formats.

--- lib_cosyc/parser/lexer.rs
+++ lib_cosyc/parser/lexer.rs
@@ -39,7 +39,16 @@ impl<'a> Lexer<'a> {
 						}
 					}
 					continue 'search
-				}
+				},
+				CharKind::LeftParen => TokenKind::LeftParen,
+                CharKind::RightParen => TokenKind::RightParen,
+                CharKind::LeftBrace => TokenKind::LeftBrace,
+                CharKind::RightBrace => TokenKind::RightBrace,
+                CharKind::SemiColon => TokenKind::SemiColon,
+                CharKind::Dollar => TokenKind::Dollar,
+                CharKind::Backtick => TokenKind::Backtick,
+                CharKind::Hashtag => TokenKind::Hashtag,
+                CharKind::Address => TokenKind::Address,
 				CharKind::EoF => TokenKind::EoF,
 				_ => return Err("unexpected symbol")
 			};
@@ -64,73 +73,9 @@ pub type Result = std::result::Result<TokenKind, &'static str>;
 /// An enum which describes available token types.
 #[derive(PartialEq, Debug, Clone)]
 pub enum TokenKind {
-	Keyword(KeywordKind),
-	Symbol(SymbolKind),
-	Identifier,
-	Operator(OperatorKind),
-	Value(ValueKind),
-	EoF
-}
-impl TokenKind {
-	/// Returns `true` if the token is a keyword.
-	pub fn is_keyword(&self) -> bool {
-		if let TokenKind::Keyword(..) = self
-				{ true } else { false }
-	}
-
-	/// Returns `true` if the token is a symbol.
-	pub fn is_symbol(&self) -> bool {
-		if let TokenKind::Symbol(..) = self
-				{ true } else { false }
-	}
-
-	/// Returns `true` if the token is an identifier.
-	pub fn is_identifier(&self) -> bool {
-		if let
-		| TokenKind::Identifier
-		| TokenKind::Operator(..) = self
-				{ true } else { false }
-	}
-
-	/// Returns `true` if the token is an operator.
-	pub fn is_operator(&self) -> bool {
-		if let TokenKind::Operator(..) = self
-				{ false } else { true }
-	}
-
-	/// Returns `true` if the token is a value.
-	pub fn is_value(&self) -> bool {
-		if let TokenKind::Value(..) = self
-				{ true } else { false }
-	}
-
-	/// Returns `true` if the token is the end of the file.
-	pub fn is_eof(&self) -> bool {
-		if let TokenKind::EoF = self
-				{ true } else { false }
-	}
-
-	/// Returns `true` if the token can start an expression.
-	pub fn is_nonterminal(&self) -> bool {
-		self.is_identifier()
-				|| self.is_value()
-				|| matches!(self,
-						Self::Symbol(SymbolKind::LeftParen) |
-						Self::Symbol(SymbolKind::Hashtag))
-	}
-}
-
-/// An enum which describes available keyword types.
-#[derive(PartialEq, Debug, Clone)]
-pub enum KeywordKind {
 	Var,
 	If,
-	Else
-}
-
-/// An enum which describes available symbol types.
-#[derive(PartialEq, Debug, Clone)]
-pub enum SymbolKind {
+	Else,
 	LeftParen,
 	RightParen,
 	LeftBrace,
@@ -139,12 +84,51 @@ pub enum SymbolKind {
 	Dollar,
 	Backtick,
 	Hashtag,
-	Address
+	Address,
+	Identifier(IdentifierKind),
+	Value(ValueKind),
+	EoF
+}
+impl TokenKind {
+	/// Returns `true` if the token is an identifier.
+	pub fn is_identifier(&self) -> bool {
+		matches!(self, TokenKind::Identifier(..))
+	}
+
+	/// Returns `true` if the token is an alphabetic identifier.
+	pub fn is_alphabetic(&self) -> bool {
+		matches!(self, TokenKind::Identifier(IdentifierKind::Alphabetic))
+	}
+
+	/// Returns `true` if the token is an operator identifier.
+	pub fn is_operator(&self) -> bool {
+		self.is_identifier() && !self.is_alphabetic()
+	}
+
+	/// Returns `true` if the token is a value.
+	pub fn is_value(&self) -> bool {
+		matches!(self, TokenKind::Value(..))
+	}
+
+	/// Returns `true` if the token is the end of the file.
+	pub fn is_eof(&self) -> bool {
+		matches!(self, TokenKind::EoF)
+	}
+
+	/// Returns `true` if the token can start an expression.
+	pub fn is_nonterminal(&self) -> bool {
+		self.is_alphabetic()
+				|| self.is_value()
+				|| matches!(self,
+						Self::LeftParen |
+						Self::Hashtag)
+	}
 }
 
 /// An enum which describes available identifier types.
 #[derive(PartialEq, Debug, Clone)]
-pub enum OperatorKind {
+pub enum IdentifierKind {
+	Alphabetic,
 	Bar,
 	Caret,
 	Ampersand,
