f57cab67fe471e52e32a56e0352b1b8f145c1899
NuxiiGit <nuxii09@gmail.com>
2020-04-08 00:45:18 +0100

    Elaborated on some basic abstract syntax.

--- lib_cosyc/parser.rs
+++ lib_cosyc/parser.rs
@@ -3,6 +3,7 @@ pub mod lexer;
 use lexer::{ Lexer, TokenKind };
 
 use crate::issues::{ Error, ErrorKind, IssueTracker };
+use crate::span::Span;
 
 /// Takes a lexer and uses it to construct a parse tree.
 pub struct Parser<'e> {
@@ -20,5 +21,40 @@ impl<'a, 'e> Parser<'e> {
 	}
 }
 
+/// A struct which stores information about the parsed program.
 #[derive(Debug)]
-pub struct Prog;
+pub struct Prog {
+	pub stmts : Block
+}
+
+/// A struct which represents a block of statements.
+pub type Block = Vec<Stmt>;
+
+/// A recursive enum which stores statement information.
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Expr {
+        expr : Expr
+    },
+    NoOp
+}
+
+/// A recursive enum which stores expression information.
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Value {
+		kind : ValueKind,
+		span : Span
+    },
+    Variable {
+        span : Span
+    },
+}
+
+/// Represents the different kinds of primitive values.
+#[derive(PartialEq, Debug, Clone)]
+pub enum ValueKind {
+	Integer,
+	Real,
+	Char
+}
