48cdefbb5e170f8dc612cc80a4cf97fcdd3adbf8
NuxiiGit <nuxii09@gmail.com>
2020-05-24 01:34:13 +0100

    Partial implementation of the error display.

--- lib_cosyc/common.rs
+++ lib_cosyc/common.rs
@@ -2,6 +2,8 @@ pub mod diagnostics;
 
 use diagnostics::IssueTracker;
 
+use std::fmt;
+
 /// A struct which stores session information, such as:
 /// - Source code
 /// - Errors
@@ -11,6 +13,15 @@ pub struct Session {
 	pub src : String,
 	/// Used to log any errors encountered during the session.
 	pub issues : IssueTracker
+}
+impl fmt::Display for Session {
+	fn fmt(&self, out : &mut fmt::Formatter) -> fmt::Result {
+		let newlines = prospect_newlines(&self.src);
+		for issue in &self.issues {
+			writeln!(out, "{:?}: {}", issue.kind, issue.reason)?;
+		}
+		Ok(())
+	}
 }
 impl From<String> for Session {
 	fn from(src : String) -> Self {
@@ -41,4 +52,9 @@ pub fn prospect_newlines(src : &str) -> Vec<usize> {
 		}
 	}
 	locations
+}
+
+/// Uses a binary search to locate the row and column number of this source location.
+pub fn infer_source_location(lines : &[usize], index : usize) -> (usize, usize) {
+	unimplemented!()
 }
--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@ fn main() {
 	let now = Instant::now();
 	let src = "((\nabc_+_coo\r(\n\r\n\r\r l)));";
 	let mut sess = Session::from(String::from(src));
-	println!("newlines : {:?}", cosyc::common::prospect_newlines(&sess.src));
-
 	let mut parser = Parser::from(&mut sess);
 	let result = parser.parse_expr();
 	match result {
@@ -25,7 +23,5 @@ fn main() {
 	}
 	let dt = now.elapsed();
 	println!("{} s / {} ms / {} Ms", dt.as_secs(), dt.as_millis(), dt.as_micros());
-	for issue in &sess.issues {
-		println!("{}", issue);
-	}
+	println!("{}", sess);
 }
