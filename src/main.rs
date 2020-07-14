17b4f3f1df4a6618ab62de6ad7b12fa2766cc3b3
NuxiiGit <nuxii09@gmail.com>
2020-07-14 17:15:45 +0100

    makes sure the lexer still works

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 /*use cosyc::{
-	parse::{
-		Parser,
-		lex::{ Lexer, TokenKind }
-	},
-	common::{
-		Session,
-		diagnostics::{ IssueTracker }
-	}
+    parse::{
+        Parser,
+        lex::{ Lexer, TokenKind }
+    },
+    common::{
+        Session,
+        diagnostics::{ IssueTracker }
+    }
 };*/
 
-use cosyc;
+use cosyc::{
+    parse::lex::{ Lexer },
+    common::Session
+};
 
 use std::time::Instant;
 
 fn main() {
-	let now = Instant::now();
-	//let mut sess = Session::read("workspace/sandbox.cosy").unwrap();
-	//let mut parser = Parser::from(&mut sess);
-	//let ast = parser.parse_program();
-	//println!("AST: {:#?}", ast);
-	let dt = now.elapsed();
-	println!("{} s / {} ms / {} Ms", dt.as_secs(), dt.as_millis(), dt.as_micros());
-	//println!("{}", sess);
+    let now = Instant::now();
+    let mut sess = Session::read("tmp/sandbox/sandbox.cosy").unwrap();
+    let mut lexer = Lexer::from(&sess.src);
+    //let mut parser = Parser::from(&mut sess);
+    //let ast = parser.parse_program();
+    //println!("AST: {:#?}", ast);
+    loop {
+        let token = lexer.advance();
+        println!("{:?}", token);
+        if token.is_eof() {
+            break;
+        }
+    }
+    let dt = now.elapsed();
+    println!("{} s / {} ms / {} Ms", dt.as_secs(), dt.as_millis(), dt.as_micros());
+    //println!("{}", sess);
 }
