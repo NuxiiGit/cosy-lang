42fcc74afc5960a33037d399262da701e1d0cf84
NuxiiGit <nuxii09@gmail.com>
2020-07-16 00:37:26 +0100

    updated tests

--- cosyc/src/main.rs
+++ cosyc/src/main.rs
@@ -4,15 +4,16 @@ use libcosyc_span::*;
 fn main() {
     let mut sess = Session::from(format!("hello world"));
     sess.filepath = format!("some_location.cosy");
-    let span = Span {
-        begin : 2,
-        end : 7
-    };
-    Diagnostic::from(&span)
+    Diagnostic::from(&Span { begin : 2, end : 7 })
             .reason(format!("just testing uwu"))
             .note(format!("alright"))
             .note(format!("but did you know that uhhhhh"))
             .level(ErrorLevel::Bug)
+            .report(&mut sess);
+    Diagnostic::from(&Span { begin : 5, end : 9 })
+            .reason(format!("another one"))
+            .note(format!("bwehhh,,,"))
+            .level(ErrorLevel::Fatal)
             .report(&mut sess);
     println!("{}", sess);
 }
