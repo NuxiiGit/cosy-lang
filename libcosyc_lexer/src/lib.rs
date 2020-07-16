a429db953f02e0b6f200abbcba3ba793fa6b0721
NuxiiGit <nuxii09@gmail.com>
2020-07-16 23:09:22 +0100

    migrated old token code

--- libcosyc_lexer/src/lib.rs
+++ libcosyc_lexer/src/lib.rs
@@ -1,1 +1,72 @@
 pub mod scanner;
+
+use scanner::{ CharReader, CharKind };
+
+use libcosyc_span::Span;
+
+/// Represents literal types.
+#[derive(PartialEq, Debug, Clone)]
+pub enum LiteralKind {
+    Integral
+}
+
+/// Represents identifier types.
+#[derive(PartialEq, Debug, Clone)]
+pub enum IdentifierKind {
+    Alphanumeric,
+    Multiplication,
+    Addition,
+    Comparison,
+    And,
+    Or,
+    Equality,
+    Application,
+    Other
+}
+
+/// Represents token types.
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenKind {
+    Literal(LiteralKind),
+    Identifier(IdentifierKind),
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    SemiColon,
+    Let,
+    Assign,
+    EoF,
+    Unknown
+}
+impl TokenKind {
+    /// Returns `true` if the token is a literal value.
+    pub fn is_literal(&self) -> bool {
+        matches!(self, Self::Literal(..))
+    }
+
+    /// Returns `true` if the token is an identifier.
+    pub fn is_identifier(&self) -> bool {
+        matches!(self, Self::Identifier(..))
+    }
+
+    /// Returns `true` if the token is an alphabetic identifier.
+    pub fn is_alphanumeric(&self) -> bool {
+        matches!(self, Self::Identifier(.., IdentifierKind::Alphanumeric))
+    }
+
+    /// Returns whether this token is a valid terminal value.
+    pub fn is_terminal(&self) -> bool {
+        self.is_literal() || self.is_identifier()
+    }
+
+    /// Returns `true` if the token is an operator identifier.
+    pub fn is_operator(&self) -> bool {
+        self.is_identifier() && !self.is_alphanumeric()
+    }
+
+    /// Returns `true` if the token is the end of the file.
+    pub fn is_eof(&self) -> bool {
+        matches!(self, Self::EoF)
+    }
+}
