a2a5c985d0a642e825671cd6e18831e253a55093
NuxiiGit <nuxii09@gmail.com>
2020-09-05 00:19:17 +0100

    set up parser dependencies

--- libcosyc_parse/src/lib.rs
+++ libcosyc_parse/src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+use libcosyc_span::Span;
+use libcosyc_lexer::Lexer;
+use libcosyc_diagnostics::Diagnostic;
