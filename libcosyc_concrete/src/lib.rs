b02d453f5320212416b0d767b1bdc9def0a48804
NuxiiGit <nuxii09@gmail.com>
2020-09-06 02:17:13 +0100

    migrated some old parser syntax

--- libcosyc_concrete/src/lib.rs
+++ libcosyc_concrete/src/lib.rs
@@ -4,6 +4,31 @@ use lex::{ Lexer, TokenKind, LiteralKind, IdentifierKind };
 
 use libcosyc_diagnostics::{ Diagnostic, Session, IssueTracker, span::Span };
 
+/// Represents a kind of terminal expression.
+#[derive(Debug)]
+pub enum TerminalKind {
+    Variable
+}
+
+/// Represents a terminal value
+#[derive(Debug)]
+pub struct Terminal {
+    span : Span,
+    kind : TerminalKind
+}
+
+/// Represents a kind of expression.
+#[derive(Debug)]
+pub enum ExprKind {
+    Terminal(Terminal)
+}
+
+/// Represents expression information
+#[derive(Debug)]
+pub struct Expr {
+    kind : ExprKind
+}
+
 /// Produces a concrete syntax tree from concrete syntax.
 pub struct Parser<'a> {
     issues : &'a mut IssueTracker,
