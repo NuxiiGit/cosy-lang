75277fa31838f672cd7e392de4eca65e1e038407
NuxiiGit <nuxii09@gmail.com>
2020-09-06 01:49:55 +0100

    set up basic parser

--- libcosyc_concrete/src/lib.rs
+++ libcosyc_concrete/src/lib.rs
@@ -1,3 +1,20 @@
 pub mod lex;
 
+use lex::{ Lexer, TokenKind, LiteralKind, IdentifierKind };
+
+use libcosyc_diagnostics::{ Diagnostic, Session, span::Span };
+
+/// Produces a concrete syntax tree from concrete syntax.
+pub struct Parser<'a> {
+    sess : &'a mut Session,
+    lexer : Lexer<'a>,
+    peeked : TokenKind
+}
+impl<'a> From<&'a mut Session> for Parser<'a> {
+        fn from(sess : &'a mut Session) -> Self {
+                let mut lexer = Lexer::from(&sess.src as &str);
+                let peeked = lexer.generate_token();
+                Self { sess, lexer, peeked }
+        }
+}
 
