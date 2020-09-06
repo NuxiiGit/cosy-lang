b2a49996ed4a86a6534cfadbff3b6c890d5bd46c
NuxiiGit <nuxii09@gmail.com>
2020-09-07 00:27:23 +0100

    back tracking

--- libcosyc_concrete/src/lib.rs
+++ libcosyc_concrete/src/lib.rs
@@ -1,10 +1,41 @@
 pub mod lex;
-pub mod syntax;
 
 use lex::{ Lexer, TokenKind, LiteralKind, IdentifierKind };
-use syntax::*;
 
 use libcosyc_diagnostics::{ Diagnostic, Session, IssueTracker, span::Span };
+
+/// Represents the different primitive variants.
+#[derive(Debug)]
+pub enum ValueKind {
+    Integral
+}
+
+/// Represents a kind of terminal expression.
+#[derive(Debug)]
+pub enum TerminalKind {
+    Variable,
+    Value(ValueKind)
+}
+
+/// Represents a terminal value
+#[derive(Debug)]
+pub struct Terminal {
+    span : Span,
+    kind : TerminalKind
+}
+
+/// Represents a kind of expression.
+#[derive(Debug)]
+pub enum ExprKind {
+    Terminal(Terminal)
+}
+
+/// Represents expression information
+#[derive(Debug)]
+pub struct Expr {
+    kind : ExprKind
+}
+
 
 /// Produces a concrete syntax tree from concrete syntax.
 pub struct Parser<'a> {
