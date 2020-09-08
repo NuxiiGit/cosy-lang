fd06ec6f4438f72485d1c2a715c759961ac4a678
NuxiiGit <nuxii09@gmail.com>
2020-09-08 15:00:25 +0100

    implemented basic desugaring

--- cosyc/src/main.rs
+++ cosyc/src/main.rs
@@ -1,13 +1,17 @@
 use libcosyc_source::Span;
 use libcosyc_diagnostics::{ Diagnostic, Session, ErrorLevel };
 use libcosyc_concrete::Parser;
+use libcosyc_abstract::Desugar;
 
 fn main() {
-    let src = "1 1_st";
-    let mut parser = Parser::from(src);
-    let syntax = parser.parse_expr_terminal();
-    println!("{:?}", syntax);
-    let mut sess = Session::from(format!("hello wo\n\n\nrld\n\n\nhihihihihihihihihih"));
+    let mut sess = Session::from(format!("18832'22"));
+    let mut parser = Parser::from(&sess.src as &str);
+    let parse_tree = parser.parse_expr_terminal();
+    let ast = parse_tree.desugar(&mut sess.issues);
+    println!("{:?}", ast);
+    println!("{}", sess);
+
+    /*let mut sess = Session::from(format!("hello wo\n\n\nrld\n\n\nhihihihihihihihihih"));
     sess.filepath = format!("some_location.cosy");
     Diagnostic::from(&Span { begin : 2, end : 5 })
             .reason(format!("just testing uwu"))
@@ -21,4 +25,5 @@ fn main() {
             .level(ErrorLevel::Fatal)
             .report(&mut sess.issues);
     println!("{}", sess);
+    */
 }
--- libcosyc_abstract/src/lib.rs
+++ libcosyc_abstract/src/lib.rs
@@ -7,7 +7,7 @@ use libcosyc_source::Span;
 use libcosyc_diagnostics::{ Diagnostic, IssueTracker, ErrorLevel };
 
 /// Provides an interface for desugaring concrete syntax into abstract syntax.
-trait Desugar {
+pub trait Desugar {
     /// The type to desugar into.
     type Out;
 
@@ -22,11 +22,21 @@ impl Desugar for concrete::Expr {
         if let Some(concrete_kind) = self.kind {
             let kind = match concrete_kind {
                 concrete::ExprKind::Variable => ExprKind::Variable,
-                _ => unimplemented!()
+                _ => {
+                    Diagnostic::from(&span)
+                            .level(ErrorLevel::Bug)
+                            .reason(format!("unsupported expression kind"))
+                            .report(issues);
+                    return None;
+                }
             };
-            Expr { span, kind }
+            Some(Expr { span, kind })
         } else {
-            unimplemented!()
+            Diagnostic::from(&span)
+                    .level(ErrorLevel::Fatal)
+                    .reason(format!("malformed expression"))
+                    .report(issues);
+            None
         }
     }
 }
