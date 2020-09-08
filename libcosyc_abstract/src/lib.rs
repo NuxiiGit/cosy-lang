8a2c20d009d64b7ce733dec3df0558265fdd9c6f
NuxiiGit <nuxii09@gmail.com>
2020-09-08 01:23:31 +0100

    added test implementation of concrete syntax Expr

--- libcosyc_abstract/src/lib.rs
+++ libcosyc_abstract/src/lib.rs
@@ -5,7 +5,13 @@ use libcosyc_source::Span;
 use libcosyc_diagnostics::{ Diagnostic, IssueTracker, ErrorLevel };
 
 /// Provides an interface for desugaring concrete syntax into abstract syntax.
-trait Desugar<'a, R> {
+trait Desugar<R> {
     /// Desugar `self` into the type `R`.
-    fn desugar(self, issues : &'a mut IssueTracker) -> R;
+    fn desugar(self, issues : &mut IssueTracker) -> R;
+}
+
+impl Desugar<syntax::Expr> for concrete::ExprKind {
+    fn desugar(self, issues : &mut IssueTracker) -> syntax::Expr {
+        unimplemented!()
+    }
 }
