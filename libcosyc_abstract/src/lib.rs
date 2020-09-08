98126f78eafe6db9b1463e5f41b39771041f8a5b
NuxiiGit <nuxii09@gmail.com>
2020-09-08 01:20:06 +0100

    provided desugar trait in abstract syntax crate

--- libcosyc_abstract/src/lib.rs
+++ libcosyc_abstract/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod syntax;
+
+use libcosyc_concrete::syntax as concrete;
+use libcosyc_source::Span;
+use libcosyc_diagnostics::{ Diagnostic, IssueTracker, ErrorLevel };
+
+/// Provides an interface for desugaring concrete syntax into abstract syntax.
+trait Desugar<'a, R> {
+    /// Desugar `self` into the type `R`.
+    fn desugar(self, issues : &'a mut IssueTracker) -> R;
+}
