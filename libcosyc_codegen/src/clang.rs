a18627d4e59df15829afa45500b7561fdb87179f
NuxiiGit <nuxii09@gmail.com>
2020-09-10 00:53:05 +0100

    added basic c codegen interface

--- libcosyc_codegen/src/clang.rs
+++ libcosyc_codegen/src/clang.rs
@@ -1,2 +1,12 @@
 use libcosyc_source::Span;
 use libcosyc_abstract::syntax;
+
+use std::fmt;
+
+/// Represents any kind of output stream.
+pub type Output<'a> = &'a mut dyn fmt::Write;
+
+/// Provides an interface for generating C code.
+pub trait CGen {
+    fn codegen(self, out : Output) -> fmt::Result;
+}
