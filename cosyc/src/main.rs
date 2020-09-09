ea7c3befb181936a897db46d9ef22440a8f062e5
NuxiiGit <nuxii09@gmail.com>
2020-09-09 01:56:26 +0100

    removed dead code

--- cosyc/src/main.rs
+++ cosyc/src/main.rs
@@ -1,5 +1,4 @@
-use libcosyc_source::Span;
-use libcosyc_diagnostics::{ Diagnostic, Session, ErrorLevel };
+use libcosyc_diagnostics::Session;
 use libcosyc_concrete::Parser;
 use libcosyc_abstract::Desugar;
 
@@ -9,23 +8,7 @@ fn main() {
  ((a)     7))
 "));
     let mut parser = Parser::from(&sess.src as &str);
-    let ast = parser.parse_expr().desugar(&mut sess.issues);
-    println!("{:?}", ast);
+    let _ast = parser.parse_expr().desugar(&mut sess.issues);
+    //println!("{:?}", ast);
     println!("{}", sess);
-
-    /*let mut sess = Session::from(format!("hello wo\n\n\nrld\n\n\nhihihihihihihihihih"));
-    sess.filepath = format!("some_location.cosy");
-    Diagnostic::from(&Span { begin : 2, end : 5 })
-            .reason(format!("just testing uwu"))
-            .note(format!("alright"))
-            .note(format!("but did you know that uhhhhh"))
-            .level(ErrorLevel::Bug)
-            .report(&mut sess.issues);
-    Diagnostic::from(&Span { begin : 5, end : 15 })
-            .reason(format!("another one"))
-            .note(format!("bwehhh,,,"))
-            .level(ErrorLevel::Fatal)
-            .report(&mut sess.issues);
-    println!("{}", sess);
-    */
 }
