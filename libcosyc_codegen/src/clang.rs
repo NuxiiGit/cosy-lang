a3f671dde99c3a0dfef53933cd077c7e280707e9
NuxiiGit <nuxii09@gmail.com>
2020-09-14 23:32:38 +0100

    refactored the codegenerator

--- libcosyc_codegen/src/clang.rs
+++ libcosyc_codegen/src/clang.rs
@@ -3,16 +3,12 @@ use libcosyc_abstract::syntax::*;
 
 use std::fmt;
 
-type Output<'a> = &'a mut dyn fmt::Write;
-
-/// Provides an interface for generating C code from abstract syntax.
-pub trait Codegen {
-    /// Write code to output stream `out`.
-    fn codegen(self, out : Output);
+/// Represents the state of the C code generator.
+pub struct Codegen<'a> {
+    out : &'a mut dyn fmt::Write
 }
-
-impl Codegen for Expr {
-    fn codegen(self, out : Output) {
-        unimplemented!()
+impl<'a> From<&'a mut dyn fmt::Write> for Codegen<'a> {
+    fn from(out : &'a mut dyn fmt::Write) -> Self {
+        Self { out }
     }
 }
