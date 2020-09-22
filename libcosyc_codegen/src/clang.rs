14d848f5905f745863d71ef1bcc47b6bc08007d3
NuxiiGit <nuxii09@gmail.com>
2020-09-22 21:24:16 +0100

    added basic C codegenerator

--- libcosyc_codegen/src/clang.rs
+++ libcosyc_codegen/src/clang.rs
@@ -7,6 +7,13 @@ use std::fmt;
 pub struct Codegen<'a> {
     out : &'a mut dyn fmt::Write
 }
+impl Codegen<'_> {
+    /// Generates a the header for a program.
+    pub fn gen_program(&mut self) -> fmt::Result {
+        write!(self.out, "void main() {{ return 0; }}")?;
+        Ok(())
+    }
+}
 impl<'a> From<&'a mut dyn fmt::Write> for Codegen<'a> {
     fn from(out : &'a mut dyn fmt::Write) -> Self {
         Self { out }
