25f0aebeba7f2cde326261ad9645c7fcc32f91fe
NuxiiGit <nuxii09@gmail.com>
2020-09-23 01:39:41 +0100

    set up main for code generator

--- cosyc/src/main.rs
+++ cosyc/src/main.rs
@@ -1,12 +1,17 @@
 use libcosyc_diagnostics::Session;
 use libcosyc_concrete::parse::Parser;
 use libcosyc_abstract::Desugar;
+use libcosyc_codegen::clang::Codegen;
 
 fn main() {
     let mut sess = Session::from("12".to_string());
     let mut parser = Parser::from(&sess.src as &str);
     let cst = parser.parse_expr();
     //println!("{:?}", cst);
-    let _ast = Desugar::from(&mut sess.issues).desugar_expr(cst);
+    if let Some(ast) = Desugar::from(&mut sess.issues).desugar_expr(cst) {
+        let mut out = String::new();
+        let mut codegen = Codegen::new(&mut out);
+        let _ = codegen.emit_expr(ast);
+    }
     println!("{}", sess);
 }
