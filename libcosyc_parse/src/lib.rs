391c8b0ad02ae4a05bb28e5c3b06c4f0cccb6fc0
NuxiiGit <nuxii09@gmail.com>
2021-01-02 04:51:22 +0000

    set up the parser signature

--- libcosyc_parse/src/lib.rs
+++ libcosyc_parse/src/lib.rs
@@ -1,1 +1,15 @@
 pub mod syntax;
+
+//use libcosyc_diagnostic::{
+//    source::Span,
+//    error::{ IssueTracker, CompilerError, ErrorLevel }
+//};
+use libcosyc_scan::{ Lexer, token::TokenKind };
+//use crate::syntax as ast;
+//use std::mem;
+
+/// Produces a concrete syntax tree from concrete syntax.
+pub struct Parser<'a> {
+    _lexer : Lexer<'a>,
+    _peeked : TokenKind
+}
