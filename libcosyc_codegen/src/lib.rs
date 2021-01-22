bde5c97eea0508941c93754696805e2615df6763
NuxiiGit <nuxii09@gmail.com>
2021-01-22 03:22:19 +0000

    salvaged old code generator code

--- libcosyc_codegen/src/lib.rs
+++ libcosyc_codegen/src/lib.rs
@@ -5,6 +5,8 @@ use libcosyc_diagnostic::{
 use libcosyc_ir::ir;
 use std::fmt::Write;
 
+const INDENTATION : &'static str = "  ";
+
 /// Manages generation of code from IR.
 pub struct Codegen<'a, W : Write> {
     src : &'a str,
@@ -32,6 +34,16 @@ impl<'a, W : Write> Codegen<'a, W> {
         Self { src, issues, out, indent }
     }
 
+    /// Increases the indentation of the output.
+    pub fn indent(&mut self) {
+        self.indent += 1;
+    }
+
+    /// Decreases the indentation of the output.
+    pub fn unindent(&mut self) {
+        self.indent -= 1;
+    }
+
     /// Writes a formatted string to the output stream.
     /// # Errors
     /// Returns `None` if there was a formatting error.
@@ -40,6 +52,13 @@ impl<'a, W : Write> Codegen<'a, W> {
             Ok(()) => Some(()),
             Err(e) => self.report(CompilerError::bug().reason(e))?
         }
+    }
+
+    /// Writes a newline to the output.
+    pub fn writeln(&mut self) -> Option<()> {
+        let mut line = String::from("\n");
+        line.push_str(&INDENTATION.repeat(self.indent));
+        self.write(line)
     }
 
     /// Generates C code for this IR instruction.
