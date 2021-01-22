d4b774ecce28c07acb4e1179909b88c37833cced
NuxiiGit <nuxii09@gmail.com>
2021-01-22 03:15:21 +0000

    added more boilerplate

--- libcosyc_codegen/src/lib.rs
+++ libcosyc_codegen/src/lib.rs
@@ -3,25 +3,47 @@ use libcosyc_diagnostic::{
     source::Renderable
 };
 use libcosyc_ir::ir;
+use std::fmt::Write;
 
 /// Manages generation of code from IR.
-pub struct Codegen<'a> {
+pub struct Codegen<'a, W : Write> {
     src : &'a str,
-    issues : &'a mut IssueTracker
+    issues : &'a mut IssueTracker,
+    out : W,
+    indent : usize
 }
 
-impl Failable for Codegen<'_> {
+impl<W : Write> Failable for Codegen<'_, W> {
     fn issues(&mut self) -> &mut IssueTracker {
         self.issues
     }
 }
 
-impl Renderable for Codegen<'_> {
+impl<W : Write> Renderable for Codegen<'_, W> {
     fn src(&self) -> &str {
         self.src
     }
 }
 
-impl Codegen<'_> {
+impl<'a, W : Write> Codegen<'a, W> {
+    /// Creates a new instance from this issue tracker and source file.
+    pub fn new(src : &'a str, issues : &'a mut IssueTracker, out : W) -> Self {
+        let indent = 0;
+        Self { src, issues, out, indent }
+    }
 
+    /// Writes a formatted string to the output stream.
+    /// # Errors
+    /// Returns `None` if there was a formatting error.
+    pub fn write<T : ToString>(&mut self, string : T) -> Option<()> {
+        match write!(self.out, "{}", string.to_string()) {
+            Ok(()) => Some(()),
+            Err(e) => self.report(CompilerError::bug().reason(e))?
+        }
+    }
+
+    /// Generates C code for this IR instruction.
+    pub fn gen_c(&mut self, inst : ir::Inst) -> Option<()> {
+        unimplemented!()
+    }
 }
