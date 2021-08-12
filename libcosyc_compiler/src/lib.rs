0b0c415a1fbeb7297e26cb1a17a3a64c63765530
NuxiiGit <nuxii09@gmail.com>
2021-08-12 01:44:51 +0100

    more assorted tests

--- libcosyc_compiler/src/lib.rs
+++ libcosyc_compiler/src/lib.rs
@@ -3,15 +3,27 @@ use libcosyc_parse as parse;
 use libcosyc_ir as cosyir;
 use libcosyc_codegen as codegen;
 
+/// Represents the output stream, in this case just a string.
+type Out = String;
+
+/// Starts a new compiler session using this file path.
+pub fn open(path : &str) -> Session {
+    Session::load(path)
+}
+
+/// Compiles this program to the desired level.
+pub fn compile(sess : &mut Session) -> Option<Out> {
+    let ast = parse::build_ast(&sess.src, &mut sess.issues)?;
+    let ir = cosyir::generate_ir(ast, &sess.src, &mut sess.issues)?;
+    let mut out = String::new();
+    codegen::generate_c(ir, &sess.src, &mut sess.issues, &mut out)?;
+    Some(out)
+}
+
 pub fn test() {
-    let mut sess = Session::load("examples/test.cosy");
-    if let Some(ast) = parse::build_ast(&sess.src as &str, &mut sess.issues) {
-        if let Some(ir) = cosyir::generate_ir(ast, &sess.src as &str, &mut sess.issues) {
-            let mut out = String::new();
-            if codegen::generate_c(ir, &sess.src as &str, &mut sess.issues, &mut out).is_some() {
-                println!("{}", out);
-            }
-        }
+    let mut sess = open("examples/test.cosy");
+    if let Some(out) = compile(&mut sess) {
+        println!("{}", out);
     }
     if sess.errors_occurred() {
         println!("{}", sess);
